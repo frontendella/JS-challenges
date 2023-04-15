74f82c191cdaaef60649f891f43043215cda1e84
frontendella <ryukhinaella@gmail.com>
2023-04-15 13:20:58 -0700

    added ValidTime

--- 09. Valid Time /main.js
+++ 09. Valid Time /main.js
@@ -1,7 +1,24 @@
-function validTime(str) {
-    //  write code here.
-}
+// Valid Time
+// Check if the given string is a correct time representation of the 24-hour clock.
+// Example
+// • For time = "13:58", the output should be validTime(time) = true;
+// • For time = "25:51", the output should be validTime(time) = false;
+// • For time = "02:76", the output should be validTime(time) = false.
+// Hints
+// • parselnt)
+// • split()
 
+
+
+
+function validTime(time) {
+    const timeParts = time.split(':');
+    const hours = parseInt(timeParts[0]);
+    const minutes = parseInt(timeParts[1]);
+    
+    return hours >= 0 && hours <= 23 && minutes >= 0 && minutes <= 59;
+  }
+  
 
 
 /**
