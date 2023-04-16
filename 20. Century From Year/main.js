4097e32e90211c2cdb771c6d6d0ee2404781ac44
frontendella <ryukhinaella@gmail.com>
2023-04-16 12:43:04 -0700

    Added solution "Century From Year"

--- 20. Century From Year/main.js
+++ 20. Century From Year/main.js
@@ -1,6 +1,25 @@
-function centuryFromYear(num) {
-    //  write code here.
-}
+// Century From Year
+// Given a year, return the century it is in. The first century spans from the year 1 up to and including the year 100, the second - from the year 101 up to and including the year 200, etc.
+// Example
+// • For year = 1905, the output should be centuryFromYear(year) = 20;
+// • For year = 1700, the output should be centuryFromYear(year) = 17.
+// Hints
+// • Math.floor()
+
+// Solution with Math.floor
+
+function centuryFromYear(year) {
+    // Divide the year by 100 and round down to the nearest integer to get the century
+    return Math.floor((year - 1) / 100) + 1;
+  }
+  
+// Solution with Math.ceil
+
+function centuryFromYear(year) {
+    // Divide the year by 100 and round up to the nearest integer to get the century
+    return Math.ceil(year / 100);
+  }
+  
 
 
 
