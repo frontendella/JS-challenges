d4d26a3ae45844da3c01d75049bc5977cc74db98
frontendella <ryukhinaella@gmail.com>
2023-04-16 11:51:09 -0700

    Added "Count Vowel Consonant"

--- 15. Count Vowel Consonant/main.js
+++ 15. Count Vowel Consonant/main.js
@@ -1,6 +1,29 @@
-function countVowelConsonant(str) {
-  // write code here
+// Count Vowel Consonant
+// You are given a strings that consists of only lowercase English letters. If vowels ('a', 'e', 'i', 'o', and 'u') are given a value of 1 and consonants are given a value of 2, return the sum of all of the letters in the input string.
+// Example
+// • For s = "abcde", the output should be count VowelConsonant(s) = 8.
+// Hints
+// • split()
+// • reduce)
+
+
+
+function countVowelConsonant(s) {
+  const vowels = ['a', 'e', 'i', 'o', 'u'];
   
+  // Split the string into an array of characters
+  const chars = s.split('');
+  
+  // Reduce the array of characters into a single value (the sum)
+  const sum = chars.reduce((acc, curr) => {
+    if (vowels.includes(curr)) {
+      return acc + 1;
+    } else {
+      return acc + 2;
+    }
+  }, 0);
+  
+  return sum;
 }
 
 
